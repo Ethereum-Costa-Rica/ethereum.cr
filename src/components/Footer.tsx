a1478215d38187f35902002ab64339697f7508cc
CodeRoostr <marcosgonzalez05750@gmail.com>
2023-04-12 12:45:08 -0600

    add: Body part I

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,29 @@
-import { Button, Text, Flex, Spacer, Box } from "@chakra-ui/react";
-import Image from 'next/image';
-import EthCrLogo from '../../public/eth-cr.svg';
+import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
-    <Flex
-      paddingTop={10}
-      px="90px"
-      direction="column"
-      justifyContent="space-evenly"
-      backgroundColor="#EDF2F7"
-      h="198px"
-    >
-      <Flex direction="row" justifyContent="space-between" alignItems="center">
-        <Box>
-          <Flex alignItems="center" mb={-3}>
-            <Text fontSize='4xl'  color="red">Ethereum</Text>
-            <Text fontSize='4xl' color="red" ml={2} as='b'>Costa Rica</Text>
-          </Flex>
-          <Text fontSize='xl' mb={5}>Creado con amor por la comunidad</Text>
-          <Text fontSize='lg'>2023</Text>
-        </Box>
-        <Button color="red" colorScheme='red' variant='outline'>Donar con crypto</Button>
+    <div>
+      <Flex
+        paddingTop={10}
+        px="90px"
+        direction="column"
+        justifyContent="space-evenly"
+        backgroundColor="#EDF2F7"
+        h="198px"
+      >
+        <Flex direction="row" justifyContent="space-between" alignItems="center">
+          <Box>
+            <Flex alignItems="center" mb={-3}>
+              <Text fontSize='4xl' color="red">Ethereum</Text>
+              <Text fontSize='4xl' color="red" ml={2} as='b'>Costa Rica</Text>
+            </Flex>
+            <Text fontSize='xl' mb={5}>Creado con amor por la comunidad</Text>
+            <Text fontSize='lg'>2023</Text>
+          </Box>
+          <Button color="red" colorScheme='red' variant='outline'>Donar con crypto</Button>
+        </Flex>
       </Flex>
-    </Flex>
+    </div>
   );
 };
 

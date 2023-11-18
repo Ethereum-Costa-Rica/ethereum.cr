fa91e73b032d8651e60d86a9c43091a720ec3b1e
CodeRoostr <marcosgonzalez05750@gmail.com>
2023-11-18 01:08:40 -0600

    feat: More speakers

--- src/components/eth-cr-day/speakers/data.ts
+++ src/components/eth-cr-day/speakers/data.ts
@@ -2,28 +2,28 @@ import { SpeakerType } from "../models";
 
 export const SpeakersData: Array<SpeakerType> = [
   {
-    name: "Perchy",
-    image: "perchy.jpg",
-    title: "Web3 cartoonist",
+    name: "Jaff Sandi",
+    image: "jaff-sandi.jpg",
+    title: "Core Team Member - Push Protocol",
     description:
-      "How NFTs are uniquely able to help artists build and unite communities around their artwork.",
-    topic: "Using NFTs to Build Community",
+      "",
+    topic: "Organizaciones Evolucionarias - un vistazo al futuro hoy",
   },
   {
-    name: "Justin Castillo",
-    image: "justin.jpg",
-    title: "Blockchain Engineer",
+    name: "Brad Harrison",
+    image: "brad-harrison.jpeg",
+    title: "Venus Protocol",
     description:
-      "Aprenderás las características del estándar ERC20 y cómo puede ayudarte a crear activos digitales",
-    topic: "Workshop: Crea tu propio token",
+      "",
+    topic: "Deployment of Venus Protocol on Ethereum Mainnet",
   },
   {
-    name: "Ranulfo",
-    image: "ranulfo.jpeg",
-    title: "Ingeniero de Software",
+    name: "Kevin Barquero",
+    image: "kevin-barquero.jpg",
+    title: "Web3 Entrepreneur",
     description:
-      "Explicar con analogias los beneficios de AA, zk-Starks y onchain worlds y como sus integraciones pueden traer billones de usuarios del mundo Web2 para Web3 usando la red de Ethereum L2.",
-    topic: "Web3 para las masas: confluencia de AA, zk-Starks y onchain worlds",
+      "",
+    topic: "Web3 for Newbies: Wallets and Dapps",
   },
   {
     name: "Igor Yalovoy",
@@ -34,18 +34,46 @@ export const SpeakersData: Array<SpeakerType> = [
     topic: "From Code to Contract: Mastering Solidity Basics",
   },
   {
-    name: "Ph.D, Edumar León Cordero",
-    image: "edumar.jpg",
-    title: "Global Content Manager de Venus Protocol",
-    description: "Periodista, Educadora y Consultora Web3",
-    topic: "Transdisciplinariedad en el acceso y adopción del ecosistema web3",
+    name: "Pedro Rey Puma",
+    image: "pedro-rey.jpg",
+    title: "Docente en el Diplomado de CriptoEconomia en Universidad de Cuyo, Fundador de CriptoLaPlata y Ethereum Bolivia",
+    description:
+      "Se enseñará los nuevos tipos de empleo que hay en web3, características, el conocimiento que deben adquirir para conseguir empleo y el ambiente del emprendimiento y creación de nuevos proyectos web3.",
+    topic:
+      "El empleo en web3 y los emprendimientos",
   },
   {
-    name: "Otto Mora",
-    image: "otto.jpg",
-    title: "Polygon ID BizDev, Asoblockchain Board Member",
-    description: "BizDev, engineering manager, consultor y lider de comunidad",
-    topic: "Identitidad Zero Knowledge y construyendo comunidad Web3",
+    name: "Justin Castillo",
+    image: "justin.jpg",
+    title: "Blockchain Engineer",
+    description:
+      "Aprenderás las características del estándar ERC20 y cómo puede ayudarte a crear activos digitales",
+    topic: "Workshop: Crea tu propio token en 45min",
+  },
+  {
+    name: "Perchy",
+    image: "perchy.jpg",
+    title: "Web3 Cartoonist",
+    description:
+      "How NFTs are uniquely able to help artists build and unite communities around their artwork.",
+    topic: "Using NFTs to Build Community",
+  },
+  {
+    name: "Alberto Galán",
+    image: "alberto-galan.jpg",
+    title: "Founder InBest Program, Partner 886Crypto",
+    description:
+      "Engineer, Educator, Blockchain Builder & User",
+    topic:
+      "Escalando Ethereum con Roll Ups y Tecnologías de Segunda Capa",
+  },
+  {
+    name: "Ranulfo",
+    image: "ranulfo.jpeg",
+    title: "Ingeniero de Software",
+    description:
+      "Explicar con analogias los beneficios de AA, zk-Starks y onchain worlds y como sus integraciones pueden traer billones de usuarios del mundo Web2 para Web3 usando la red de Ethereum L2.",
+    topic: "Web3 para las masas: confluencia de AA, zk-Starks y onchain worlds",
   },
   {
     name: "Daniel Bejarano",
@@ -58,32 +86,17 @@ export const SpeakersData: Array<SpeakerType> = [
   {
     name: "Jose Miguel Zamora",
     image: "josemiguel.jpg",
-    title: "Socio Fundador Lighthouse",
+    title: "Abogado - Socio Fundador Lighthouse",
     description:
-      "Abogado enfocado en fintech, blockchain y servicios financieros",
-    topic: "El Abogado crypto",
+      "",
+    topic: "Donde estamos y para donde vamos: Regulación Costa Rica",
   },
   {
-    name: "Karla Cordoba",
-    image: "karla.jpg",
-    title: "Founder ReFi Costa Rica",
-    description:
-      "Blockchain, Impact & ReFi - Cambiatus Co Founder - Founder ReFi Costa Rica ",
-    topic: "Intro a Governanza, DAOs, CofiBlocks",
-  },
-  {
-    name: "Robert Ramirez",
-    image: "robert.jpg",
-    title: "Fundador Check My Ticket",
-    description: "Desarrollador y emprendedor",
-    topic: "El Futuro de la Venta de Entradas de Eventos con NFTs",
-  },
-  {
-    name: "Rafael Campos",
-    image: "rafael.jpg",
-    title: "Desarrollador en Zero Knowledge",
-    description: "Desarrollador en Zero Knowledge",
-    topic: "Desarrollando zk apps con o1js",
+    name: "Gabriel Carranza",
+    image: "gabriel-carranza.jpeg",
+    title: "Web3 and AI Enthusiast",
+    description: "",
+    topic: "Consenso Inteligente: El Futuro de las DAOs, Impulsado por la IA",
   },
   {
     name: "Edgar Barrantes",
@@ -95,21 +108,88 @@ export const SpeakersData: Array<SpeakerType> = [
       "Developer onboarding: herramientas para el desarrollo en Ethereum y L2s",
   },
   {
-    name: "Alberto Galán",
-    image: "alberto-galan.jpg",
-    title: "Founder InBest Program, Partner 886Crypto",
+    name: "Karla Cordoba",
+    image: "karla.jpg",
+    title: "Founder ReFi Costa Rica",
     description:
-      "Engineer, Educator, Blockchain Builder & User",
-    topic:
-      "Escalando Ethereum con Roll Ups y Tecnologías de Segunda Capa",
+      "Blockchain, Impact & ReFi - Cambiatus Co Founder - Founder ReFi Costa Rica ",
+    topic: "Negocios Colaborativos y ReFi: el experimento de CofiBlocks",
   },
   {
-    name: "Pedro Rey Puma",
-    image: "pedro-rey.jpg",
-    title: "Docente en el Diplomado de CriptoEconomia en Universidad de Cuyo, Fundador de CriptoLaPlata y Ethereum Bolivia",
-    description:
-      "Se enseñará los nuevos tipos de empleo que hay en web3, características, el conocimiento que deben adquirir para conseguir empleo y el ambiente del emprendimiento y creación de nuevos proyectos web3.",
-    topic:
-      "El empleo en web3 y los emprendimientos",
+    name: "Robert Ramirez",
+    image: "robert.jpg",
+    title: "Fundador Check My Ticket",
+    description: "Desarrollador y emprendedor",
+    topic: "El Futuro de la Venta de Entradas de Eventos con NFTs",
+  },
+  {
+    name: "Andres Obando",
+    image: "andres-obando.jpg",
+    title: "Senior Developer EY Blockchain",
+    description: "En esta charla, exploraremos el concepto de pruebas de conocimiento cero y cómo pueden abordar los desafíos actuales en seguridad y privacidad de los datos en las redes de blockchain. Analizaremos cómo estas pruebas ofrecen una solución innovadora para garantizar la confidencialidad de la información. Acompáñanos en descubrir cómo las pruebas de conocimiento cero protegen los datos en Blockchain.",
+    topic: "Blockchain y Pruebas de Conocimiento Cero (ZKP): Una combinación perfecta para garantizar la privacidad y la transparencia",
+  },
+  {
+    name: "Hugo Villalta",
+    image: "hugo-villalta.jpg",
+    title: "Manager EY Blockchain",
+    description: "En esta charla, exploraremos el concepto de pruebas de conocimiento cero y cómo pueden abordar los desafíos actuales en seguridad y privacidad de los datos en las redes de blockchain. Analizaremos cómo estas pruebas ofrecen una solución innovadora para garantizar la confidencialidad de la información. Acompáñanos en descubrir cómo las pruebas de conocimiento cero protegen los datos en Blockchain.",
+    topic: "Blockchain y Pruebas de Conocimiento Cero (ZKP): Una combinación perfecta para garantizar la privacidad y la transparencia",
+  },
+  {
+    name: "Otto Mora",
+    image: "otto.jpg",
+    title: "Polygon ID BizDev, Asoblockchain Board Member",
+    description: "BizDev, engineering manager, consultor y lider de comunidad",
+    topic: "Intro a IdentidadAutosoberana y Credenciales Verificables",
+  },
+  {
+    name: "Fabricio Espinoza",
+    image: "fabricio-espinoza.jpg",
+    title: "CEO enotar.io",
+    description: "",
+    topic: "Notariado Digital: un nuevo paradigma con Blockchain. Casos de uso reales",
+  },
+  {
+    name: "Rafael Campos",
+    image: "rafael.jpg",
+    title: "Desarrollador en Zero Knowledge",
+    description: "Desarrollador en Zero Knowledge",
+    topic: "Desarrollando zk apps con o1js",
+  },
+  {
+    name: "Mario Hernandez",
+    image: "mario-hernandez.jpg",
+    title: "CEO y cofundador Impesa",
+    description: "",
+    topic: "Evolución de Monibyte y Cryptowallet",
+  },
+  {
+    name: "Francis Gomez",
+    image: "francis-gomez.jpg",
+    title: "Cofundador Sakundi",
+    description: "",
+    topic: "Tikuna: AI aplicada a la detección de ataques en el Blockchain",
+  },
+  {
+    name: "Francis Sanchinelli",
+    image: "francis-sanchinelli.jpeg",
+    title: "Entrepreneur, Scrum Master, Strategy & Innovation",
+    description: "",
+    topic: "Panel de discusion",
+  },
+  {
+    name: "Leo Lara",
+    image: "leo-lara.jpg",
+    title: "Protocol Research Engineer, Ethereum Protocol Fellow",
+    description: "",
+    topic: "Panel de discusion",
+  },
+  {
+    name: "Ilan Melendez",
+    image: "ilan-melendez.jpg",
+    title: "Líder de proyecto Blockchain, LACChain",
+    description: "",
+    topic: "Panel de discusion",
   },
 ];

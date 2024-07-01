4e78202e88edda7b9cfa6a06ba39a47d016a3a5d
JustinCast <justincastillovalladares@gmail.com>
2024-06-30 20:34:50 -0600

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,12 @@
-import { Box, Flex, Text, Input, Button, useTheme } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Text,
+  Input,
+  Button,
+  useTheme,
+  Link,
+} from "@chakra-ui/react";
 import { useTranslation } from "next-i18next";
 import Image from "next/image";
 
@@ -11,25 +19,35 @@ const Footer = () => {
       <Text variant="h1-semibold">{t("footer.contactUs")}</Text>
 
       <Flex direction="row" justifyContent="space-between" alignItems="center">
-        <Image src="assets/x-icon.svg" width={64} height={64} alt="X icon" />
+        <Link href="https://x.com/EthereumCR" target="_blank">
+          <Image src="assets/x-icon.svg" width={64} height={64} alt="X icon" />
+        </Link>
         <Image
           src="assets/facebook-icon.svg"
           width={64}
           height={64}
           alt="Facebook icon"
         />
-        <Image
-          src="assets/linkedin-icon.svg"
-          width={64}
-          height={64}
-          alt="Linkedin icon"
-        />
-        <Image
-          src="assets/meetup-icon.svg"
-          width={64}
-          height={64}
-          alt="Meetup icon"
-        />
+
+        <Link
+          href="https://www.linkedin.com/company/ethereum-costa-rica"
+          target="_blank"
+        >
+          <Image
+            src="assets/linkedin-icon.svg"
+            width={64}
+            height={64}
+            alt="Linkedin icon"
+          />
+        </Link>
+        <Link href="https://www.meetup.com/es-ES/ethereumcr/" target="_blank">
+          <Image
+            src="assets/meetup-icon.svg"
+            width={64}
+            height={64}
+            alt="Meetup icon"
+          />
+        </Link>
       </Flex>
       <Flex direction="column" gap="10px">
         <Text variant="h1-semibold">{t("footer.keepInTouch")}</Text>
@@ -63,6 +81,7 @@ const Footer = () => {
           as="a"
           href="https://www.linkedin.com/in/angie-herrera-aguilar/"
           color="blue"
+          target="_blank"
         >
           Designed by Angie Herrera
         </Text>

79381dc6dce7cc87a847864e1143649389b4b030
JustinCast <justincastillovalladares@gmail.com>
2024-09-04 21:22:01 -0600

    feat: add buy tickets button

--- src/components/event/cover-mobile.tsx
+++ src/components/event/cover-mobile.tsx
@@ -3,12 +3,14 @@ import {
   // Box,
   useTheme,
   Flex,
-  // Button,
-  // Icon,
-  // Text,
+  Button,
+  Icon,
+  Text,
 } from "@chakra-ui/react";
+import { t } from "i18next";
 import Image from "next/image";
 import { useTranslation } from "react-i18next";
+import { MdArrowForward } from "react-icons/md";
 // import { MdArrowForward } from "react-icons/md";
 
 type CoverProps = {
@@ -17,7 +19,7 @@ type CoverProps = {
 
 const CoverMobile = ({ coverGraphicUrl }: CoverProps) => {
   const theme = useTheme();
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <Flex
@@ -36,10 +38,17 @@ const CoverMobile = ({ coverGraphicUrl }: CoverProps) => {
           alt="ETH Pura Vida Hero"
         />
       </Flex>
-      {/* <Button disabled={true} mt="4px" variant="eth-pura-vida-yellow" w="100%">
+      <Button
+        as="a"
+        href="https://www.hallos.io/event/ethereum-pura-vida-2024"
+        target="_blank"
+        mt="4px"
+        variant="eth-pura-vida-yellow"
+        w="100%"
+      >
         <Text as="span">{t("ethPuraVida.buyTickets")}</Text>
         <Icon w="24px" h="24px" as={MdArrowForward} ml="10px" />
-      </Button> */}
+      </Button>
     </Flex>
   );
 };
